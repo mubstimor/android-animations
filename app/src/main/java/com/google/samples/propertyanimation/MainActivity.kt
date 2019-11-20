093fa235f1ccb6fbb88259001da8c7dfafe1c492
Timothy Mubiru <mubstimor@gmail.com>
2019-11-20 09:30:39 +0300

    add rotate, translate and scale animations

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -16,10 +16,16 @@
 
 package com.google.samples.propertyanimation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -70,12 +76,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rotater() {
+        // Rotate the view for a second around its center once
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION,
+            -360f, 0f)
+        animator.duration = 1000
+        animator.disableViewDuringAnimation(rotateButton)
+        animator.start()
     }
 
     private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X,
+            200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(translateButton)
+        animator.start()
     }
 
     private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(
+            star, scaleX, scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
     }
 
     private fun fader() {
@@ -85,6 +111,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun shower() {
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
     }
 
 }
